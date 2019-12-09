ac89f5dffac3304ef019affc3dae8309da34919e
Ben Jones <ben.james.jones@gmail.com>
2019-12-09 15:02:16 -0700

    dec9

--- cpu.d
+++ cpu.d
@@ -4,104 +4,133 @@ import std.range;
 
 struct IOPort{
     alias data this;
-    int[] data;
+    long[] data;
 };
 
 struct CPU{
 
-    int[] array;
+    long[long] memory;
     IOPort *input;
     IOPort *output;
-    int pc;
+    long pc;
+    long relativeOffset; 
     enum RetCode{ Halted, Blocked};
 
+    this(long[] insts, IOPort* inp, IOPort* outp){
+        foreach(i, inst; insts){
+            memory[i] = inst;
+        }
+        input = inp;
+        output = outp;
+        pc = 0;
+        relativeOffset = 0;
+    }
+
+    private long readMem(long addr){
+        if(addr !in memory)
+            memory[addr] = 0;
+        return memory[addr];
+    }
+    
     RetCode run(){
 
         while(true){
             //    writeln("pc: ", pc, " arr: ", array);
-            int opcode = array[pc] % 100;
+            long inst = readMem(pc);
+            int opcode = inst % 100;
+            long modes = inst /100;
+                        
             if(opcode == 99) return RetCode.Halted;
        
-            int modes = array[pc] /100;
 
-            int mode1 = modes % 10;
-            int mode2 = (modes /10) % 10;
-            int mode3 = modes / 100;
+
+            long readOp(int op){
+                int mode = (modes / 10^^ (op -1)) % 10;
+                switch(mode){
+                    case 0: //position based
+                        return readMem(readMem(pc + op));
+                    case 1: //immediate
+                        return readMem(pc + op);
+                    case 2: //relative offset
+                        return readMem(relativeOffset + readMem(pc + op));
+                    default:
+                        assert(false);
+                }
+            }
+
+            long realAddress(int op){
+                int mode = (modes / 10^^ (op -1)) % 10;
+                switch(mode){
+                    case 0:
+                        return readMem(pc + op);
+                    case 1:
+                        assert(false); //immediate.. doesn't make sense
+                    case 2:
+                        return relativeOffset + readMem(pc + op);
+                    default:
+                        assert(false);
+                }
+            }
 
             //writeln("running: ", array[pc], " m1 ", mode1, " m2 ", mode2, " m3 ", mode3);
 
             switch(opcode){
                 case 1: //plus
-                    assert(mode3 == 0);
-                    int op1 = mode1 == 1 ? array[pc +1] : array[array[pc +1]];
-                    int op2 = mode2 == 1 ? array[pc +2] : array[array[pc +2]];
-                    array[array[pc +3]] = op1 + op2;
+                    memory[realAddress(3)] = readOp(1) + readOp(2);
                     pc += 4;
                     break;
                 case 2: //times
-                    assert(mode3 == 0);
-                    int op1 = mode1 == 1 ? array[pc +1] : array[array[pc +1]];
-                    int op2 = mode2 == 1 ? array[pc +2] : array[array[pc +2]];
-                    array[array[pc +3]] = op1 * op2;
+                    memory[realAddress(3)] = readOp(1) * readOp(2);
                     pc += 4;
                     break;
 
 
                 case 3: //input
-                    assert(mode1 == 0);
                     if(input.empty)
                         return RetCode.Blocked;
-                    array[array[pc +1]] = (*input)[0];
+                    memory[realAddress(1)] = (*input)[0];
                     *input = (*input)[1.. $];
                     pc += 2;
                     break;
 
                 case 4:
-                    int toPrint = mode1 == 1 ? array[pc +1] : array[array[pc +1]];
+                    long toPrint = readOp(1);
                     writeln("output: ", toPrint);
                     *output ~= toPrint;
                     pc += 2;
                     break;
 
                 case 5:  //jump if true
-                    int op1 = mode1 == 1 ? array[pc +1] : array[array[pc +1]];
-                    int op2 = mode2 == 1 ? array[pc +2] : array[array[pc +2]];
-                    if(op1 != 0){
-                        pc = op2;
+                    if(readOp(1) != 0){
+                        pc = readOp(2);
                     } else {
                         pc += 3;
                     }
                     break;
 
                 case 6: //jump if false
-                    int op1 = mode1 == 1 ? array[pc +1] : array[array[pc +1]];
-                    int op2 = mode2 == 1 ? array[pc +2] : array[array[pc +2]];
-                    if(op1 == 0){
-                        pc = op2;
+                    if(readOp(1) == 0){
+                        pc = readOp(2);
                     } else {
                         pc += 3;
                     }
                     break;
 
                 case 7: //less than
-                    int op1 = mode1 == 1 ? array[pc +1] : array[array[pc +1]];
-                    int op2 = mode2 == 1 ? array[pc +2] : array[array[pc +2]];
-
-                    assert(mode3 == 0);
-                    array[array[pc +3]] = op1 < op2 ? 1 : 0;
-               
+                    memory[realAddress(3)] = readOp(1) < readOp(2) ? 1 : 0;
                     pc += 4;
                     break;
 
                 case 8: //equals
-                    int op1 = mode1 == 1 ? array[pc +1] : array[array[pc +1]];
-                    int op2 = mode2 == 1 ? array[pc +2] : array[array[pc +2]];
-
-                    assert(mode3 == 0);
-                    array[array[pc +3]] = op1 == op2 ? 1 : 0;
-
+                    memory[realAddress(3)] = readOp(1) == readOp(2) ? 1 : 0;
                     pc += 4;
                     break;
+
+                case 9: //change relpos
+                    relativeOffset += readOp(1);
+                    pc += 2;
+                    break;
+                    
                 default:
                     assert(false);
                
